7cfca380768abcbc91cac89c36381657e642ec09
John Stack <johnfstack@gmail.com>
2024-08-04 13:04:56 +0100

    Portfolio

---
+++ portfolio/slideshow.js
@@ -0,0 +1,10 @@
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+  .fadeOut(0)
+  .next()
+  .fadeIn(0)
+  .end()
+  .appendTo('#slideshow');
+}, 1000);
